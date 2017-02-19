044e26157745985746734bd1233c2f0974d4ab32
cremyos <dreamer_forever@126.com>
2017-02-19 23:48:01 +0800

    param module

---
+++ 1-drivermodel/1-2modules/smodule.mod.c
@@ -0,0 +1,34 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x5c7202ab, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xf296b1f1, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0xa0f78192, __VMLINUX_SYMBOL_STR(param_ops_bool) },
+	{ 0x14347e09, __VMLINUX_SYMBOL_STR(param_ops_byte) },
+	{ 0xc26d6104, __VMLINUX_SYMBOL_STR(param_ops_charp) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "EBC130D62390445915D8E56");
